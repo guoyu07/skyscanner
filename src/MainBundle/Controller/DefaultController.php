76b0a0489e03e595cf6e7578c19f6fe72e60b1e9
Javier García FIllol <javier.garcia.fillol@gmail.com>
2017-04-17 11:20:03 +0200

    Adding serialize and changes on new entity Skyscanner

--- src/MainBundle/Controller/DefaultController.php
+++ src/MainBundle/Controller/DefaultController.php
@@ -5,17 +5,43 @@ namespace MainBundle\Controller;
 
 use MainBundle\Entity\Skyscanner;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\HttpFoundation\Response;
 
 
 class DefaultController extends Controller
 {
     public function indexAction()
     {
-        $sky = new Skyscanner("http://partners.api.skyscanner.net/apiservices","ca648373878536744215063892044569");
-         // es-ES
-        //$response = $sky->getLocales();
-        $respone = $sky->get('/reference/v1.0/currencies');
-        return $this->render('MainBundle::index.html.twig');
+        $sky = new Skyscanner("http://partners.api.skyscanner.net/apiservices", "ca648373878536744215063892044569");
+
+        // es-ES
+        // $response = $sky->getLocales();
+        // EUR
+        // $response = $sky->get('/reference/v1.0/currencies');
+
+        // ES / MX
+        // $response = $sky->get('/reference/v1.0/countries/es-ES');
+
+        // BCN = BARC
+        // MX = MEXA
+        // $response = $sky->get('/geo/v1.0');
+
+        $info = array(
+            'country' => 'ES',
+            'currency' => 'EUR',
+            'locale' => 'es-ES',
+            'originPlace' => 'BARC',
+            'destinationPlace' => 'MXA',
+            'outboundDate' => '2017-05-15',
+            'inboundDate' => '2017-06-15',
+            'cabinClass' => 'economy',
+            'adults' => '1',
+            'apiKey' => 'ca648373878536744215063892044569'
+        );
+        $response = $sky->post('/pricing/v1.0',$info);
+
+        $data = json_encode($response);
+        return new Response($data);
     }
 
 
--- src/MainBundle/Entity/Skyscanner.php
+++ src/MainBundle/Entity/Skyscanner.php
@@ -10,6 +10,8 @@ class Skyscanner
     private $username;
     private $password;
     private $headers;
+    private $headerPost;
+    private $headerPostFile;
     private $baseUrl;
     private $key;
     private $curl;
@@ -22,20 +24,30 @@ class Skyscanner
             'Accept: application/json',
             'Content-Type: application/json'
         );
+
+        $this->headerPost = array(
+            'Accept: application/json',
+            'Content-Type: application/x-www-form-urlencoded'
+        );
+
+        $this->headerPostFile = array(
+            'X-Atlassian-Token: nocheck',
+            'Content-Type: multipart/form-data'
+        );
     }
 
-    private function configureCurl($url)
+    private function configureCurl()
     {
         //Configure Curl
         $this->curl = curl_init();
         curl_setopt($this->curl, CURLOPT_VERBOSE, 1);
-        curl_setopt($this->curl, CURLOPT_HTTPHEADER, $this->headers);
         curl_setopt($this->curl, CURLOPT_RETURNTRANSFER, 1);
-        $this->setUrl($url);
+        curl_setopt($this->curl, CURLOPT_FOLLOWLOCATION, 1);
+        curl_setopt($this->curl, CURLOPT_HEADER, true);
 
         // Optional Authentication:
-        // curl_setopt($this->curl, CURLOPT_SSL_VERIFYPEER, 0);
-        // curl_setopt($this->curl, CURLOPT_SSL_VERIFYHOST, 0);
+        curl_setopt($this->curl, CURLOPT_SSL_VERIFYPEER, 0);
+        curl_setopt($this->curl, CURLOPT_SSL_VERIFYHOST, 0);
         // curl_setopt($curl, CURLOPT_HTTPAUTH, CURLAUTH_BASIC);
         // curl_setopt($curl, CURLOPT_USERPWD, $this->username . ":" . $this->password);
     }
@@ -65,35 +77,67 @@ class Skyscanner
         return "?apiKey=" . $this->key;
     }
 
-    private function setUrl($url)
+    private function setUrlKey($url)
     {
         $fullUrl = $this->baseUrl . $url . $this->insertKey();
         curl_setopt($this->curl, CURLOPT_URL, $fullUrl);
     }
 
-    private function resturnValue($result)
+    private function setUr($url)
     {
-        if (($resultObject = json_decode($result)) != null) {
-            return $resultObject;
-        } else {
-            return $result;
-        }
+        $fullUrl = $this->baseUrl . $url;
+        curl_setopt($this->curl, CURLOPT_URL, $fullUrl);
     }
 
-    public function getLocales()
+    private function resturnValue()
     {
-        $this->configureCurl('/reference/v1.0/locales');
         $response = curl_exec($this->curl);
+
+        $header_size = curl_getinfo($this->curl, CURLINFO_HEADER_SIZE);
+        $header = substr($response, 0, $header_size);
+        $body = substr($response, $header_size);
+
         curl_close($this->curl);
-        return $this->resturnValue($response);
+        echo $header . '<br><br>';
+
+        if (($resultObject = json_decode($body)) != null) {
+            return $resultObject;
+        } else {
+            return $body;
+        }
     }
 
     public function get($url)
     {
-        $this->configureCurl($url);
-        $response = curl_exec($this->curl);
-        curl_close($this->curl);
-        return $this->resturnValue($response);
+        $this->configureCurl();
+        $this->setUrlKey($url);
+
+        curl_setopt($this->curl, CURLOPT_HTTPHEADER, $this->headers);
+
+        return $this->resturnValue();
+    }
+
+    public function post($url, $data)
+    {
+        $i = 0;
+        $d = count($data);
+        $raw = "";
+        foreach ($data as $key => $item) {
+            $raw .= $key . '=' . $item;
+            $i++;
+            if ($i < $d) {
+                $raw .= '&';
+            }
+        }
+
+        $this->configureCurl();
+        $this->setUr($url);
+        curl_setopt($this->curl, CURLOPT_HTTPHEADER, $this->headerPost);
+        curl_setopt($this->curl, CURLOPT_POST, true);
+        curl_setopt($this->curl, CURLOPT_POSTFIELDS, $raw);
+
+
+        return $this->resturnValue();
     }
 
     public function postJSONFile($url, $file_path)
@@ -106,13 +150,7 @@ class Skyscanner
         $data = array('file' => $cfile); //creating array for Curl Post Fields
         $ch = curl_init(); // initiating curl
 
-//setting the headers
-        $headers = array(
-            'X-Atlassian-Token: nocheck',
-            'Content-Type: multipart/form-data'
-        );
-
-//setting curl option on array
+        //setting curl option on array
         curl_setopt_array($ch, array(
             CURLOPT_URL => $url,
             CURLOPT_USERPWD => $this->username . ":" . $this->password,
@@ -122,10 +160,10 @@ class Skyscanner
             CURLOPT_VERBOSE => 1,
             CURLOPT_POSTFIELDS => $data,
             CURLOPT_RETURNTRANSFER => true,
-            CURLOPT_HTTPHEADER => $headers,
+            CURLOPT_HTTPHEADER => $this->headerPostFile,
         ));
 
-//executing curl
+        //executing curl
         $response = curl_exec($ch);
         curl_close($ch); //closing curl
 
